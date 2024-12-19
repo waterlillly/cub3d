d45dc5096b1340b8448ce9243e042cbe317c5aa0
Monika Simic <msimic@c3r10p1.42vienna.com>
2024-12-19 19:30:32 +0100

    main separation xD

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include "../cub3d.h"
 #include "math/calc.h"
 
+void args_handler(int ac, char **av)
+{
+	int arg_len;
+
+	arg_len = ft_strlen(av[1]);
+	if (ac != 2)
+		err("Error\nInvalid number of arguments\n", NULL);
+	else if (arg_len <= 4)
+		err("Error\nInvalid file name\n", NULL);
+	else if (!ft_strnstr(&av[1][arg_len - 4], ".cub", 4))
+		err("Error\nInvalid file extension\n", NULL);
+}
+
 int	main(int ac, char **av)
 {
 	t_pars		pars;
@@ -9,6 +22,10 @@ int	main(int ac, char **av)
 	// int		i;
 	// bool	done;
 
+	/******MO******/
+	args_handler(ac, av);
+
+	/******LI******/
 	ft_bzero(&pars, sizeof(t_pars *));
 	init_struct(&pars);
 	if (ac == 2 && av[1])
