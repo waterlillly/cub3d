c6cc45da3cc3d756258cce97042e0bfe82a7ae4c
Monika Simic <msimic@c3r10p5.42vienna.com>
2025-01-23 15:38:41 +0100

    FIX: player orientation, player count and the invalid character

--- src/parsing/check_map_elements.c
+++ src/parsing/check_map_elements.c
@@ -17,7 +17,6 @@ void check_elements(t_game *game)
     int row;
     int column;
     int players = 0;
-    char orientation = '0';
 
     row = 0;
     while (game->data.map[row])
@@ -25,9 +24,12 @@ void check_elements(t_game *game)
         column = 0;
         while (game->data.map[row][column])
         {
-            if (ft_strchr("NSEW", game->data.map[row][column]) && orientation == '0'){
-                orientation = game->data.map[row][column];
-				transfer_orient(orientation, game);
+            if (!ft_strchr(" 01NSEW", game->data.map[row][column])){
+                printf("%c\n", game->data.map[row][column]);
+                exit_failure("Invalid character in map", game);
+            }
+            if (ft_strchr("NSEW", game->data.map[row][column])){
+				transfer_orient(game->data.map[row][column], game);
                 game->player.pos.x = row;
                 game->player.pos.y = column;
                 players++;
@@ -38,5 +40,4 @@ void check_elements(t_game *game)
     }
     if (players != 1)
         exit_failure("Player missing or too many players", game);
-    printf("PLAYER ORIENTATION: %c\n", orientation);
 }
