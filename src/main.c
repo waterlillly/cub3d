2bcc3d681281b80694bb1587fda6d81fadfd7c43
Hexelillyfee <131495080+waterlillly@users.noreply.github.com>
2025-01-25 18:32:34 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,15 @@
 #include "../cub3d.h"
 
-int	main(int ac, char **av)
+int	main(void)
 {
 	t_game	game;
 
 	ft_bzero(&game, sizeof(t_game));
-	initialization_of_vars(&game);
-	args_handler(ac, av, &game);
-	validator(&game, av);
 	init_cub(&game);
 	if (mlx_handler(&game) != 0)
 		exit_failure("mlx_handler", &game);
+	mlx_do_key_autorepeatoff(game.cub.mlx_con);
 	mlx_loop(game.cub.mlx_con);
+	mlx_do_key_autorepeaton(game.cub.mlx_con);
 	return (exit_success(&game));
 }
