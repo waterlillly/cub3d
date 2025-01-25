0974b914ab215574288e373d452bfffea4fcc003
Hexelillyfee <131495080+waterlillly@users.noreply.github.com>
2025-01-25 18:47:34 +0100

    Update movement.c

--- src/math/movement.c
+++ src/math/movement.c
@@ -1,17 +1,18 @@
 #include "../../cub3d.h"
 
+//TODO: change hardcoded values to move_speed and turn_speed!
 void	move_forward(t_game *game)
 {
-	double	new_x;
-	double	new_y;
+	double			new_x;
+	double			new_y;
 
-	// if (game->control.forward_velo < 5)
-	// 	game->control.forward_velo += 1;
-	// printf("forward: %f\n", game->control.forward_velo);
+	if (game->control.forward_velo < 3)
+		game->control.forward_velo += 1;
+	printf("forward: %f\n", game->control.forward_velo);
 	new_x = game->player.pos.x + game->player.dir.x
-		* game->player.move_speed;
+		* game->control.forward_velo;
 	new_y = game->player.pos.y + game->player.dir.y
-		* game->player.move_speed;
+		* game->control.forward_velo;
 	if (!is_wall(game, new_x, game->player.pos.y))
 		game->player.pos.x = new_x;
 	if (!is_wall(game, game->player.pos.x, new_y))
@@ -23,13 +24,14 @@ void	move_backward(t_game *game)
 	double	new_x;
 	double	new_y;
 
-	// if (game->control.backward_velo < 5)
-	// 	game->control.backward_velo += 1;
-	// printf("backward: %f\n", game->control.backward_velo);
+	
+	if (game->control.backward_velo < 3)
+		game->control.backward_velo += 1;
+	printf("backward: %f\n", game->control.backward_velo);
 	new_x = game->player.pos.x - game->player.dir.x
-		* game->player.move_speed;
+		* game->control.backward_velo;
 	new_y = game->player.pos.y - game->player.dir.y
-		* game->player.move_speed;
+		* game->control.backward_velo;
 	if (!is_wall(game, new_x, game->player.pos.y))
 		game->player.pos.x = new_x;
 	if (!is_wall(game, game->player.pos.x, new_y))
@@ -41,11 +43,11 @@ void	move_left(t_game *game)
 	double	new_x;
 	double	new_y;
 
-	// if (game->control.left_velo < 5)
-	// 	game->control.left_velo += 1;
-	// printf("left: %f\n", game->control.left_velo);
-	new_x = game->player.pos.x + game->player.dir.y * game->player.move_speed;
-	new_y = game->player.pos.y - game->player.dir.x * game->player.move_speed;
+	if (game->control.left_velo < 3)
+		game->control.left_velo += 1;
+	printf("left: %f\n", game->control.left_velo);
+	new_x = game->player.pos.x + game->player.dir.y * game->control.left_velo;
+	new_y = game->player.pos.y - game->player.dir.x * game->control.left_velo;
 	if (!is_wall(game, new_x, game->player.pos.y))
 		game->player.pos.x = new_x;
 	if (!is_wall(game, game->player.pos.x, new_y))
@@ -57,11 +59,11 @@ void	move_right(t_game *game)
 	double	new_x;
 	double	new_y;
 
-	// if (game->control.right_velo < 5)
-	// 	game->control.right_velo += 1;
-	// printf("right: %f\n", game->control.right_velo);
-	new_x = game->player.pos.x - game->player.dir.y * game->player.move_speed;
-	new_y = game->player.pos.y + game->player.dir.x * game->player.move_speed;
+	if (game->control.right_velo < 3)
+		game->control.right_velo += 1;
+	printf("right: %f\n", game->control.right_velo);
+	new_x = game->player.pos.x - game->player.dir.y * game->control.right_velo;
+	new_y = game->player.pos.y + game->player.dir.x * game->control.right_velo;
 	if (!is_wall(game, new_x, game->player.pos.y))
 		game->player.pos.x = new_x;
 	if (!is_wall(game, game->player.pos.x, new_y))
